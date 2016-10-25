d944f6ad812812a6ffa357e82b0b6e518b4d3c94
ulin666 <ulin666@gmail.com>
2016-10-25 20:49:10 +0200

    ViewHelper modifications

--- BackboardLib/ViewHelper.cs
+++ BackboardLib/ViewHelper.cs
@@ -5,10 +5,25 @@ namespace com.tumblr.backboard
 {
 	public static class ViewHelper
 	{
-		public static Android.Util.Property GetViewStaticProperty(this View view, String name)
+		private static Android.Util.Property GetViewStaticProperty(String name)
 		{
 			Property prop = Property.Of(Java.Lang.Class.FromType(typeof(View)), Java.Lang.Class.FromType(typeof(Java.Lang.Float)), name);
 			return prop;
+		}
+
+		public static Android.Util.Property TranslationX
+		{
+			get { return GetViewStaticProperty("TranslationX"); }
+		}
+
+		public static Android.Util.Property TranslationY
+		{
+			get { return GetViewStaticProperty("TranslationY"); }
+		}
+
+		public static Android.Util.Property Alpha
+		{
+			get { return GetViewStaticProperty("Alpha"); }
 		}
 	}
 }
